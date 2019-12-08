cef9016689247f7cd262594d3c258a22575ad26f
Chris Severns <severnsc@gmail.com>
2019-12-08 10:51:05 -0500

    controller extraction

--- index.js
+++ index.js
@@ -12,10 +12,10 @@ const STOP = "stop";
 let id = 0;
 let intervals = [];
 
-wss.on("connection", function connection(ws) {
-  ws.on("message", function incoming(req) {
-    const json = JSON.parse(req);
-    if (json.type === START) {
+function incoming(req) {
+  const json = JSON.parse(req);
+  switch (json.type) {
+    case START: {
       id += 1;
       let msState = json.ms;
       const interval = setInterval(() => {
@@ -23,14 +23,22 @@ wss.on("connection", function connection(ws) {
         ws.send(JSON.stringify({ ms: msState, id }));
       }, 1000);
       intervals.push({ id, interval });
+      break;
     }
-    if (json.type === STOP) {
+    case STOP: {
       const { interval } = intervals.find(i => i.id === json.id);
       clearInterval(interval);
       intervals = intervals.filter(i => i.id !== json.id);
       ws.send("stopped");
+      break;
     }
-  });
+    default:
+      ws.send("invalid type");
+  }
+}
+
+wss.on("connection", function connection(ws) {
+  ws.on("message", incoming);
 
   ws.send("something");
 });
