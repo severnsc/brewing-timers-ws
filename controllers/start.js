7a2c572cb7fa729b65a53e20cdfa945cbe809475
Chris Severns <severnsc@gmail.com>
2019-12-08 15:38:39 -0500

    Rename queue to enqueue

--- controllers/start.js
+++ controllers/start.js
@@ -1,11 +1,11 @@
-const makeStart = queueId => async (req, res) => {
+const makeStart = enqueueId => async (req, res) => {
   const id = req.id;
   if (!id) {
     res.send("must include id on request object");
   }
   try {
-    const queued = await queueId(id);
-    res.send(queued.message);
+    const enqueued = await enqueueId(id);
+    res.send(enqueued.message);
   } catch (e) {
     console.error(e);
     res.send(e.message);
