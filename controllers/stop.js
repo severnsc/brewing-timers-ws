297c8c480478745cbf10e6dbf5575149b281c567
Chris Severns <severnsc@gmail.com>
2019-12-08 15:15:49 -0500

    Add stop controller

---
+++ controllers/stop.js
@@ -0,0 +1,15 @@
+const makeStop = deQueueId => async (req, res) => {
+  const id = req.id;
+  if (!id) {
+    res.send("must include id on request object");
+  }
+  try {
+    const dequeued = await deQueueId(id);
+    res.send(dequeued.message);
+  } catch (e) {
+    console.error(e);
+    res.send(e.message);
+  }
+};
+
+export default makeStop;
