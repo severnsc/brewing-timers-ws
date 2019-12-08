e15177e4ebdc85ed918f8a5c9a3f682fba6bbeb6
Chris Severns <severnsc@gmail.com>
2019-12-08 15:44:52 -0500

    Change mock apis

--- use-cases/index.js
+++ use-cases/index.js
@@ -2,19 +2,19 @@ import makeDecrementTimer from "./decrement-timer";
 import makeEnqueue from "./enqueue";
 import makeDequeue from "./dequeue";
 
-const timersDb = {
+const timersDb = Object.freeze({
   findById: () => Promise.resolve(),
   update: () => Promise.resolve()
-};
-
-const queueResponse = Object.freeze({
-  getMessage: () => "message"
 });
 
-const queue = {
+const queueResponse = Object.freeze({
+  message: "message"
+});
+
+const queue = Object.freeze({
   enqueue: () => Promise.resolve(queueResponse),
   dequeue: () => Promise.resolve(queueResponse)
-};
+});
 
 const decrementTimer = makeDecrementTimer({ timersDb });
 const enqueue = makeEnqueue({ queue });
