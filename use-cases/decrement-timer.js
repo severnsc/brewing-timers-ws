429606aefae46c28ba2f02998cbac0515d60ee90
Chris Severns <severnsc@gmail.com>
2019-12-08 12:26:20 -0500

    Add decrement use case

---
+++ use-cases/decrement-timer.js
@@ -0,0 +1,31 @@
+import makeTimer from "../timer";
+const makeDecrementTimer = ({ timersDb }) => {
+  return async function decrementTimer({ id }) {
+    if (!id) {
+      throw new Error("You must supply a timer id");
+    }
+    const existing = await timersDb.findById({ id });
+
+    if (!existing) {
+      throw new RangeError("Timer not found!");
+    }
+
+    const timer = makeTimer({ existing });
+
+    if (timer.getRemainingDuration() <= 0) {
+      return timer;
+    }
+
+    const decremented = timer.decrement();
+
+    const updated = timersDb.update({
+      id: decremented.getId(),
+      duration: decremented.getDuration(),
+      remainingDuration: decremented.getRemainingDuration()
+    });
+
+    return decremented;
+  };
+};
+
+export default makeDecrementTimer;
