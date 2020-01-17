5e19bc8f1ee8e811ba33e551bef7ef570ce3d65a
Chris Severns <severnsc@gmail.com>
2020-01-16 19:06:03 -0500

    Fix syntax errors

--- controllers/cron.js
+++ controllers/cron.js
@@ -1,14 +1,14 @@
 const makeCron = ({ listQueue, decrementTimer }) => async () => {
-    try {
-        const list = await listQueue()
-        list.forEach({ id, res } => {
-            const decremented = await decrementTimer({ id })
-            res.send(decremented.remainingDuration)
-        })
-    } catch(e) {
-        console.error(e)
-        res.send(e.message)
-    }
-}
+  try {
+    const list = await listQueue();
+    list.forEach(async ({ id, res }) => {
+      const decremented = await decrementTimer({ id });
+      res.send(decremented.remainingDuration);
+    });
+  } catch (e) {
+    console.error(e);
+    res.send(e.message);
+  }
+};
 
-module.exports = makeCron
+module.exports = makeCron;
