9cf6d09fb1a3e083c3bbb4c40415905a0051aa97
Chris Severns <severnsc@gmail.com>
2020-01-22 18:00:12 -0500

    Todo is now done!

--- index.js
+++ index.js
@@ -22,7 +22,6 @@ const wss = new WebSocket.Server({ port: 8080 });
 //Use Cases - Getting, Decrementing
 //Controller - req/res layer, start, stop, cronjob
 //Frameworks & Drivers - websockets, cache, persistence, cron
-//TODO: adjust mocks so that we can acutally test with a client
 
 wss.on("connection", function connection(ws) {
   const cronJob = new CronJob("* * * * * *", () => timerController.cron(ws));
