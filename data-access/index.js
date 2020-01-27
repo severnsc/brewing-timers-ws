76c4131507d1612503eedccc437d000648bc676a
Chris Severns <severnsc@gmail.com>
2020-01-27 18:08:15 -0500

    Connect DAL to Hasura

--- data-access/index.js
+++ data-access/index.js
@@ -1,11 +1,66 @@
 const makeIdQueue = require("./queue");
 const makeTimersDb = require("./timers-db");
+const ApolloClient = require("apollo-boost").default;
+const gql = require("graphql-tag");
+require("cross-fetch/polyfill");
+
+const findTimerByIdQuery = gql`
+  query FindTimerById($id: uuid) {
+    __typename
+    timers(where: { id: { _eq: $id } }) {
+      id
+      duration
+      remaining_duration
+    }
+  }
+`;
+
+const updateTimerMutation = gql`
+  mutation MyMutation($id: uuid, $remaining_duration: Int) {
+    __typename
+    update_timers(
+      where: { id: { _eq: $id } }
+      _set: { remaining_duration: $remaining_duration }
+    ) {
+      returning {
+        id
+        duration
+        remaining_duration
+      }
+    }
+  }
+`;
+
+const client = new ApolloClient({
+  uri: process.env.GRAPHQL_API
+});
 
 async function makeDb() {
   return {
-    findById: async () => null,
-    update: async () => {}
+    findById,
+    update
   };
+  async function findById(id) {
+    return client
+      .query({
+        query: findTimerByIdQuery,
+        variables: { id }
+      })
+      .then(({ timers }) => timers[0])
+      .catch(error => console.error(error));
+  }
+  async function update(timer) {
+    return client
+      .mutate({
+        mutation: updateTimerMutation,
+        variables: {
+          id: timer.id,
+          remaining_duration: timer.remaining_duration
+        }
+      })
+      .then(({ update_timers: { returning } }) => returning[0])
+      .catch(error => console.error(error));
+  }
 }
 
 async function makeQueue() {
