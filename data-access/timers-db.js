edc23a88b8d487e2cfc4859c4824edb792128941
Chris Severns <severnsc@gmail.com>
2020-01-26 20:30:14 -0500

    Add trimers db DAL

---
+++ data-access/timers-db.js
@@ -0,0 +1,16 @@
+export default function makeTimersDb({ makeDb }) {
+  return Object.freeze({
+    findById,
+    update
+  });
+  async function findById({ id }) {
+    const db = await makeDb();
+    const result = await db.findById(id);
+    return result;
+  }
+  async function update(timer) {
+    const db = await makeDb();
+    const result = await db.update(timer);
+    return result;
+  }
+}
