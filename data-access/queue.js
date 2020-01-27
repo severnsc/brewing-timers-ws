af3703a44b98d616bc18481920f816db63e08708
Chris Severns <severnsc@gmail.com>
2020-01-26 20:40:06 -0500

    Add queue DAL

---
+++ data-access/queue.js
@@ -0,0 +1,28 @@
+export default function makeIdQueue({ makeQueue }) {
+  return Object.freeze({
+    enqueue,
+    dequeue,
+    get
+  });
+  async function enqueue(id) {
+    const queue = await makeQueue();
+    const isEnqueued = await queue.enqueue(id);
+    const message = isEnqueued ? "Enqueue successful" : "Enqueue failed";
+    return {
+      getMessage: () => message
+    };
+  }
+  async function dequeue(id) {
+    const queue = await makeQueue();
+    const isDequeued = await queue.dequeue(id);
+    const message = isDequeued ? "Dequeue successful" : "Dequeue failed";
+    return {
+      getMessage: () => message
+    };
+  }
+  async function get() {
+    const queue = await makeQueue();
+    const result = await queue.findAll();
+    return result;
+  }
+}
