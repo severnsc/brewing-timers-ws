5f6894986133e64b03919c66b0742b0730dcfc8f
Chris Severns <severnsc@gmail.com>
2020-03-21 17:10:14 -0400

    Update todo

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const wss = new WebSocket.Server({ port: 8080 });
 //Use Cases - Getting, Decrementing
 //Controller - req/res layer, start, stop, cronjob
 //Frameworks & Drivers - websockets, cache, persistence, cron
-//TODO: Fix the timer cache to use array instead of object
+//TODO: clear queue on close
 const envVars = {
   GRAPHQL_API: process.env.GRAPHQL_API,
   CLIENT_ID: process.env.CLIENT_ID,
