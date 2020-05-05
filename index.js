9b58790b97babb8cf610bbf8e17014431bca65fa
Chris Severns <severnsc@gmail.com>
2020-05-05 19:02:03 -0400

    Make port a config var

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const timerController = require("./controllers");
 const WebSocket = require("ws");
 
-const wss = new WebSocket.Server({ port: 8080 });
+const wss = new WebSocket.Server({ port: process.env.PORT });
 
 //Happy path: Receive a starting number of ms,
 //decrement by 1000 ms every 1000 ms until 0
