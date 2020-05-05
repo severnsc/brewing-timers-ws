0d7fb0fdf36cbcdff04b88375b90544dc58dba3e
Chris Severns <severnsc@gmail.com>
2020-05-05 19:53:51 -0400

    Remove all the unnecessary crap

--- data-access/index.js
+++ data-access/index.js
@@ -1,37 +1,6 @@
 const makeIdQueue = require("./queue");
 const makeTimersDb = require("./timers-db");
-const ApolloClient = require("apollo-boost").default;
-const gql = require("graphql-tag");
-const tokenStorage = require("./tokenStorage");
-require("cross-fetch/polyfill");
 
-const updateTimerMutation = gql`
-  mutation MyMutation($id: uuid, $remaining_duration: Int) {
-    __typename
-    update_timers(
-      where: { id: { _eq: $id } }
-      _set: { remaining_duration: $remaining_duration }
-    ) {
-      returning {
-        id
-        duration
-        remaining_duration
-      }
-    }
-  }
-`;
-
-const client = new ApolloClient({
-  uri: process.env.GRAPHQL_API,
-  request: async (operation) => {
-    const token = await tokenStorage.getToken();
-    operation.setContext({
-      headers: {
-        authorization: token ? `Bearer ${token}` : "",
-      },
-    });
-  },
-});
 let queue = {};
 async function makeStopDb() {
   return {
@@ -42,25 +11,7 @@ async function makeStopDb() {
     return Promise.resolve(queue[id]);
   }
   async function update(timer) {
-    return client
-      .mutate({
-        mutation: updateTimerMutation,
-        variables: {
-          id: timer.id,
-          remaining_duration: timer.remainingDuration,
-        },
-      })
-      .then(
-        ({
-          data: {
-            update_timers: { returning },
-          },
-        }) => {
-          const returnedTimer = returning[0];
-          return returnedTimer;
-        }
-      )
-      .catch((error) => console.error(error));
+    return Promise.resolve(true);
   }
 }
 async function makeDecrementDb() {
