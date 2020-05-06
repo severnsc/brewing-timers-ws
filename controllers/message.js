265b462790563e4bde2167ef1a007969ce9cfca9
Chris Severns <severnsc@gmail.com>
2020-05-06 19:04:46 -0400

    return early if error

--- controllers/message.js
+++ controllers/message.js
@@ -4,10 +4,10 @@ const STOP = "stop";
 const makeMessage = ({ startController, stopController }) => (req, res) => {
   const type = req.type;
   if (!type) {
-    res.send("Request must include type");
+    return res.send("Request must include type");
   }
   if (![START, STOP].includes(type)) {
-    res.send("Request type must be one of either start or stop");
+    return res.send("Request type must be one of either start or stop");
   }
   if (type === START) {
     startController(req, res);
